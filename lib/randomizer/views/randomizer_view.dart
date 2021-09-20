3d3b697713379ad92c7efc61e2f15bad9e94a2db
Alexander Terekhov <alxtrkhv@gmail.com>
2021-09-20 22:14:16 +0300

    refactor:  optimize randomizer view a bit

--- lib/randomizer/views/randomizer_view.dart
+++ lib/randomizer/views/randomizer_view.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../bloc/randomizer_bloc.dart';
-import '../../ui/input_field_with_label.dart';
 
 class RandomizerView extends StatelessWidget {
   const RandomizerView({Key? key}) : super(key: key);
@@ -18,29 +17,8 @@ class RandomizerView extends StatelessWidget {
           child: Column(
         children: [
           Padding(
-            padding: const EdgeInsets.only(top: 100, bottom: 50),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                InputFieldWithLabel(
-                  width: 75,
-                  height: 100,
-                  label: 'Min',
-                  controller: TextEditingController(text: '${bloc.state.min}'),
-                  onSubmitted: (input) =>
-                      bloc.add(RandomizerRangeUpdated(min: int.parse(input))),
-                ),
-                InputFieldWithLabel(
-                  width: 75,
-                  height: 100,
-                  label: 'Max',
-                  controller: TextEditingController(text: '${bloc.state.max}'),
-                  onSubmitted: (input) =>
-                      bloc.add(RandomizerRangeUpdated(max: int.parse(input))),
-                ),
-              ],
-            ),
-          ),
+              padding: const EdgeInsets.only(top: 100, bottom: 50),
+              child: _InputRange(bloc: bloc)),
           BlocBuilder<RandomizerBloc, RandomizerState>(
             bloc: bloc,
             builder: (_, state) => Text(
@@ -57,6 +35,86 @@ class RandomizerView extends StatelessWidget {
           onPressed: () => bloc.add(RandomizerValueRequested()),
           child: const Icon(Icons.radar),
         ),
+      ),
+    );
+  }
+}
+
+class _InputRange extends StatelessWidget {
+  _InputRange({
+    Key? key,
+    required this.bloc,
+  }) : super(key: key);
+
+  final RandomizerBloc bloc;
+
+  @override
+  Widget build(BuildContext context) {
+    const minLabel = 'Min';
+    const maxLabel = 'Max';
+
+    const double fieldWidth = 75;
+    const double fieldHeigth = 100;
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _InputFieldWithLabel(
+          width: fieldWidth,
+          height: fieldHeigth,
+          label: minLabel,
+          controller: TextEditingController(text: '${bloc.state.min}'),
+          onSubmitted: (input) =>
+              bloc.add(RandomizerRangeUpdated(min: int.parse(input))),
+        ),
+        _InputFieldWithLabel(
+          width: fieldWidth,
+          height: fieldHeigth,
+          label: maxLabel,
+          controller: TextEditingController(text: '${bloc.state.max}'),
+          onSubmitted: (input) =>
+              bloc.add(RandomizerRangeUpdated(max: int.parse(input))),
+        ),
+      ],
+    );
+  }
+}
+
+class _InputFieldWithLabel extends StatelessWidget {
+  final String label;
+  final Function(String) onSubmitted;
+  final TextEditingController controller;
+
+  final double width;
+  final double height;
+
+  _InputFieldWithLabel({
+    Key? key,
+    required this.controller,
+    required this.label,
+    required this.onSubmitted,
+    required this.width,
+    required this.height,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return SizedBox(
+      width: width,
+      height: height,
+      child: Column(
+        children: [
+          Text(
+            '$label',
+            style: theme.textTheme.headline6,
+          ),
+          TextField(
+            textAlign: TextAlign.center,
+            onSubmitted: onSubmitted,
+            controller: controller,
+          )
+        ],
       ),
     );
   }
